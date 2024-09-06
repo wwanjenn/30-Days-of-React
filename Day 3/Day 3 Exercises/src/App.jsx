d8e2e87a3bb3211e4140daec676b8f38cf50fb32
Lemonade7 <wmaglangit1118@gmail.com>
2024-09-06 19:29:01 +0800

    done with tech stacks card

---
+++ Day 3/Day 3 Exercises/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import FrontEndTechCard from './Cards/FrontEndTech/FrontEndTechCard'
+
+function App() {
+
+  return (
+    <>
+      <FrontEndTechCard></FrontEndTechCard>
+    </>
+  )
+}
+
+export default App
---
+++ Day 3/Day 3 Exercises/src/Cards/User/UserCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserCard = () =>{
+    
+}
+
+export default UserCard;
